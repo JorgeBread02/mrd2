ac1225dbb8d78ea8c19f7bfc460f64e6c19d4b7e
Jorge <jorgebreadgames@gmail.com>
2018-09-16 23:30:21 -0500

    rest server_usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,23 @@
 //puerto
 //================================================
 process.env.PORT=process.env.PORT||3000;
+//===============
+//Entorno
+//===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===============
+//DB
+//===============
+let urlDB;
+if(process.env.NODE_ENV==='dev'){
+  urlDB= 'mongodb://localhost:27017/Cafe';
+
+}else{
+    urlDB= 'mongodb://cafe-user:Jorge75432560@ds117539.mlab.com:17539/cafe'
+
+
+
+}
+process.env.URLDB=urlDB;
+
